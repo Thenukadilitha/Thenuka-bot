27c4ec6c678c03b178fe5431ebaf0e1bcb97ebfd
Thenukadilitha <thenukadilitha123@gmail.com>
2025-11-13 10:47:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "FZlChDbL#J6a_qUf0VLQJIiu3k87Z3igz_z4KYtnCKQoDCuXtMzM",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Thenukadilitha/Thenuka-bot/blob/main/Images/IMG-20251020-WA0002.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 THENUKA-BOT Is Alive Now😍*",
 BOT_OWNER: '94779400412',  // Replace with the owner's phone number
